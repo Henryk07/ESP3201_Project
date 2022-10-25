fcf0e7e24167c06681d569ea334de10a51108de5
valeriemej <e1019928@u.nus.edu>
2022-10-25 16:55:23 +0800

    final version of convert to bw ready to be used

--- convect_im2bw.py
+++ convect_im2bw.py
@@ -6,6 +6,14 @@ import glob
 directory="S_datas_labels" # ENTER HERE THE SOURCE DIRECTORY PATH
 goaldirec="S_datas_labels_bw" #ENTER HERE THE GOAL DIRECTORY PATH (has to be created, with its sub-folders F,M,N...)
 
+os.mkdir("S_datas_labels_bw")
+os.mkdir("S_datas_labels_bw/M")
+os.mkdir("S_datas_labels_bw/N")
+os.mkdir("S_datas_labels_bw/V")
+os.mkdir("S_datas_labels_bw/F")
+os.mkdir("S_datas_labels_bw/S")
+os.mkdir("S_datas_labels_bw/Q")
+
 for pathdir in glob.glob(directory+"/*"):
     dirname=os.path.normpath(pathdir)
     dirlab=os.path.basename(os.path.normpath(dirname))
@@ -18,4 +26,4 @@ for pathdir in glob.glob(directory+"/*"):
             gray = im.convert("L")
             bw = gray.point(lambda x: 0 if x < 200 else 255, 'L')
             rootpath=os.path.basename(os.path.normpath(imgname))
-            bw.save(goaldirec+"//"+dirlab+"//bw_"+rootpath)
+            bw.save("S_datas_labels_bw//"+dirlab+"//bw_"+rootpath)
