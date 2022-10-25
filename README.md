c48a7d6d5e23f944b9f764c327756b6baf3da802
Henryk Chan <e0977821@nus.edu.sg>
2022-10-25 19:13:33 +0800

    rearrgae the dataset structure and upload the black and write version of the small dataset

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ Haolin Chen, Mejsnarova Valerie
 ### for Machine Learning Purpose and testing as well.
 
 everything will appear, just relax and try to fix it
+
+### Meaning of each dataset
+
+Small_dataset:
+
+S_data_labels_cb_bw means with label combined train and test together
+and in black and write
+
+S_prediction_datas_labels means with label for prediction
+
+S_prediction_datas_labels_bw means with label and in black and write for prediction
+
+S_testing_datas_labels means with label not combined train and test together
+
+S_training_datas_labels means with label not combined train and test together
--- convect_im2bw.py
+++ convect_im2bw.py
@@ -1,29 +1,30 @@
 import os
 from PIL import Image
-import glob 
+import glob
 
 
-directory="S_datas_labels" # ENTER HERE THE SOURCE DIRECTORY PATH
-goaldirec="S_datas_labels_bw" #ENTER HERE THE GOAL DIRECTORY PATH (has to be created, with its sub-folders F,M,N...)
+directory = "S_prediction_datas_labels"  # ENTER HERE THE SOURCE DIRECTORY PATH
+# ENTER HERE THE GOAL DIRECTORY PATH (has to be created, with its sub-folders F,M,N...)
+goaldirec = "S_prediction_datas_labels_bw"
 
-os.mkdir("S_datas_labels_bw")
-os.mkdir("S_datas_labels_bw/M")
-os.mkdir("S_datas_labels_bw/N")
-os.mkdir("S_datas_labels_bw/V")
-os.mkdir("S_datas_labels_bw/F")
-os.mkdir("S_datas_labels_bw/S")
-os.mkdir("S_datas_labels_bw/Q")
+# os.mkdir("S_training_datas_labels_bw")
+os.mkdir("S_prediction_datas_labels_bw/M")
+os.mkdir("S_prediction_datas_labels_bw/N")
+os.mkdir("S_prediction_datas_labels_bw/V")
+os.mkdir("S_prediction_datas_labels_bw/F")
+os.mkdir("S_prediction_datas_labels_bw/S")
+os.mkdir("S_prediction_datas_labels_bw/Q")
 
 for pathdir in glob.glob(directory+"/*"):
-    dirname=os.path.normpath(pathdir)
-    dirlab=os.path.basename(os.path.normpath(dirname))
+    dirname = os.path.normpath(pathdir)
+    dirlab = os.path.basename(os.path.normpath(dirname))
 
     for imgpath in glob.glob(dirname+"/*.png"):
 
-        imgname=os.path.normpath(imgpath)
+        imgname = os.path.normpath(imgpath)
 
         with Image.open(imgname) as im:
             gray = im.convert("L")
             bw = gray.point(lambda x: 0 if x < 200 else 255, 'L')
-            rootpath=os.path.basename(os.path.normpath(imgname))
-            bw.save("S_datas_labels_bw//"+dirlab+"//bw_"+rootpath)
+            rootpath = os.path.basename(os.path.normpath(imgname))
+            bw.save("S_prediction_datas_labels_bw//"+dirlab+"//bw_"+rootpath)
