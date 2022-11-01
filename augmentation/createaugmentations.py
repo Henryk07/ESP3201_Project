e10149fcc6fae67b24977065b9cbfa75132d510c
Henryk Chan <e0977821@nus.edu.sg>
2022-11-01 11:00:46 +0800

    upload the version suitable for my coms

--- augmentation/createaugmentations.py
+++ augmentation/createaugmentations.py
@@ -7,6 +7,7 @@ import numpy as np
 import os
 import random
 
+
 def augmentationdata(n, directorypath, sourcedirectory):  # enter source directory
 
     # ENTER HERE THE SOURCE DIRECTORY PATH
@@ -29,55 +30,62 @@ def augmentationdata(n, directorypath, sourcedirectory):  # enter source directo
         nb_images = len(img_path_list)
         img_path_list_dest = []
         print(dirlab)
-        randused=[]
-        if (len(img_path_list))>n:
-            i=0
-            while i<int(abs(n/2)):
+        randused = []
+        if (len(img_path_list)) > n:
+            i = 0
+            while i < int(abs(n/2)):
                 ran = random.randrange(nb_images)
                 if img_path_list[ran] not in randused:
                     randused.append(img_path_list[ran])
-                    i+=1
+                    i += 1
                     img = Image.open(img_path_list[ran])
                     imgname = os.path.normpath(img_path_list[ran])
                     rootpath = os.path.basename(os.path.normpath(imgname))
-                    img.save(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"//"+rootpath)
-                    img_path_list_dest = glob.glob(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
+                    img.save(directorypath+"/S_datas_labels_augmented" +
+                             str(n)+"/"+dirlab+"//"+rootpath)
+                    img_path_list_dest = glob.glob(
+                        directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
         else:
-            for i in range(len(img_path_list)): #copy all existing images to new directory
+            # copy all existing images to new directory
+            for i in range(len(img_path_list)):
                 img = Image.open(img_path_list[i])
                 imgname = os.path.normpath(img_path_list[i])
                 rootpath = os.path.basename(os.path.normpath(imgname))
-                img.save(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"//"+rootpath)
-                img_path_list_dest = glob.glob(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
-        j=0
-        while len(img_path_list_dest) != n: #complete new directory with augmented images 
-            j+=1 #reference of the augmentation
+                img.save(directorypath+"/S_datas_labels_augmented" +
+                         str(n)+"/"+dirlab+"//"+rootpath)
+                img_path_list_dest = glob.glob(
+                    directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
+        j = 0
+        while len(img_path_list_dest) != n:  # complete new directory with augmented images
+            j += 1  # reference of the augmentation
             ran = random.randrange(nb_images)
             im = Image.open(img_path_list[ran])
 
-            #pre-processing the image for the AugMix augmentations
+            # pre-processing the image for the AugMix augmentations
             rgb_image = im.convert('RGB')
             rgbarray = np.asarray(rgb_image)
-            rgbfloat=rgbarray.astype(np.float32)
+            rgbfloat = rgbarray.astype(np.float32)
 
-            #Applying the Augmix augmentations
-            augmentedimg=am.augment_and_mix(rgbfloat)
-            
-            #Converting back the image to an array to be saved
-            augarray=Image.fromarray(augmentedimg.astype('uint8'), 'RGB')
-            augarray=augarray.convert('L') #convert to black and white
+            # Applying the Augmix augmentations
+            augmentedimg = am.augment_and_mix(rgbfloat)
 
-            #Name of the image processing
+            # Converting back the image to an array to be saved
+            augarray = Image.fromarray(augmentedimg.astype('uint8'), 'RGB')
+            augarray = augarray.convert('L')  # convert to black and white
+
+            # Name of the image processing
             imgname = os.path.normpath(img_path_list[ran])
             rootpath = os.path.basename(os.path.normpath(imgname))
 
-            #Saving the image
-            augarray.save(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"//"+rootpath+"aug"+str(j)+".png")
-            img_path_list_dest = glob.glob(directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
+            # Saving the image
+            augarray.save(directorypath+"/S_datas_labels_augmented" +
+                          str(n)+"/"+dirlab+"//"+rootpath+"aug"+str(j)+".png")
+            img_path_list_dest = glob.glob(
+                directorypath+"/S_datas_labels_augmented"+str(n)+"/"+dirlab+"/*.png")
 
 
 # ENTER
 # first argument : number of images in each category (should be 803)
 # second argument : location of the new directory
 # third argument : location of the source directory (including the parent directory)
-augmentationdata(10, "Small_dataset", "Small_dataset/S_datas_labels_cb_bw")
+augmentationdata(5000, "M_dataset", "Full_dataset/ECG_Image_data_cb_bw")
